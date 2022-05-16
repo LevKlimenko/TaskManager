14ad733b3f158b1cd2f204d369986264f0a30ced
Лев Клименко <klimenko.lev@yandex.ru>
2022-05-16 14:54:12 +0300

    Убраны ненужные элементы

--- src/ru/mywork/taskmanager/service/InMemoryTaskManager.java
+++ src/ru/mywork/taskmanager/service/InMemoryTaskManager.java
@@ -15,7 +15,6 @@ public class InMemoryTaskManager implements TaskManager {
     private final HashMap<Integer, Epic> epics = new HashMap<>();
     private final HashMap<Integer, Subtask> subtasks = new HashMap<>();
     private final HashMap<Integer, Task> tasks = new HashMap<>();
-
     private HistoryManager historyManager = Managers.getDefaultHistory();
     private int generatorId = 0;
 
