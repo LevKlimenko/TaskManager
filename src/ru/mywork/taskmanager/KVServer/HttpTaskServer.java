88c6cb208fb4b35f57312a2421e8fee61dbcdc7b
LevKlimenko <klimenko.lev@yandex.ru>
2022-09-05 00:40:23 +0300

    HomeWork

--- src/ru/mywork/taskmanager/KVServer/HttpTaskServer.java
+++ src/ru/mywork/taskmanager/KVServer/HttpTaskServer.java
@@ -2,10 +2,7 @@ package ru.mywork.taskmanager.KVServer;
 
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
-import ru.mywork.taskmanager.model.Epic;
-import ru.mywork.taskmanager.model.Subtask;
 import ru.mywork.taskmanager.model.Task;
 import ru.mywork.taskmanager.service.FileBackedTaskManager;
 import ru.mywork.taskmanager.service.Managers;
@@ -14,8 +11,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
 
 public class HttpTaskServer {
 
@@ -23,17 +23,131 @@ public class HttpTaskServer {
     private static String hostName;
     private static int port;
     private static FileBackedTaskManager fbtm = Managers.getFileBackedTaskManager();
-    private static Gson gson = new Gson();
+    private static Gson gson = Managers.getGson();
 
-    public static void main(String[] args) throws IOException {
+
+    public void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/tasks/", new TaskHandler());
-        server.createContext("/tasks/task", new TaskHandler());
-        server.createContext("/tasks/subtask", new TaskHandler());
-        server.createContext("/tasks/epic", new TaskHandler());
+        //server.createContext("/tasks/", new TaskHandler());
+        // server.createContext("/tasks/task", new TaskHandler());
+        // server.createContext("/tasks/subtask", new TaskHandler());
+        // server.createContext("/tasks/epic", new TaskHandler());
+        server.createContext("/tasks", this::handler);
     }
 
-    static class TaskHandler implements HttpHandler {
+
+    public void handler(HttpExchange httpExchange) {
+        try {
+            System.out.println("\n/tasks: " + httpExchange.getRequestURI());
+            final String path = httpExchange.getRequestURI().getPath().substring(7);
+            switch (path) {
+                case "" -> {
+                    if (!httpExchange.getRequestMethod().equals("GET")) {
+                        System.out.println("/ Ждёт GET-запрос, а получил: " + httpExchange.getRequestMethod());
+                        httpExchange.sendResponseHeaders(405, 0);
+                    }
+                    final String response = gson.toJson(fbtm.getSortedTasks());
+                    sendText(httpExchange, response);
+                }
+                case "task":
+                    handleTask(httpExchange);
+                case "subtask":
+                    handleSubtask(httpExchange);
+                case "subtask/epic":
+                    handleSubtasksByEpic(httpExchange);
+                case "epic":
+                    handleEpic(httpExchange);
+                case "history": {
+                    if (!httpExchange.getRequestMethod().equals("GET")) {
+                        System.out.println("/history Ждёт GET-запрос, а получил: " + httpExchange.getRequestMethod());
+                        httpExchange.sendResponseHeaders(405, 0);
+                    }
+                    final String response = gson.toJson(fbtm.getHistory());
+                    sendText(httpExchange, response);
+                }
+                default: {
+                    System.out.println("Неизвестный запрос: " + httpExchange.getRequestURI());
+                    httpExchange.sendResponseHeaders(404, 0);
+                }
+
+            }
+            httpExchange.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleTask(HttpExchange httpExchange) throws IOException {
+        final String query = httpExchange.getRequestURI().getQuery();
+        switch (httpExchange.getRequestMethod()) {
+            case "GET": {
+                if (query == null) {
+                    final HashMap<Integer, Task> tasks = fbtm.getTasks();
+                    final String response = gson.toJson(tasks);
+                    System.out.println("Получили все задачи");
+                    sendText(httpExchange, response);
+                    return;
+                }
+                String idParam = query.substring(3); //?id=
+                final int id = Integer.parseInt(idParam);
+                final Task task = fbtm.getTaskById(id);
+                final String response = gson.toJson(task);
+                System.out.println("Получили задачу id=" + id);
+                sendText(httpExchange, response);
+            }
+            case "DELETE": {
+                if (query == null) {
+                    fbtm.clearTask();
+                    System.out.println("Удалили все задачи");
+                    httpExchange.sendResponseHeaders(200, 0);
+                    return;
+                }
+                String idParam = query.substring(3);
+                final int id = Integer.parseInt(idParam);
+                fbtm.deleteTaskById(id);
+                System.out.println("Удалили задачу id=" + id);
+                httpExchange.sendResponseHeaders(200, 0);
+            }
+            case "POST": {
+                String json = readText(httpExchange);
+                if (json.isEmpty()) {
+                    System.out.println("Body с задачей пустой. Указывается в теле запроса");
+                    httpExchange.sendResponseHeaders(400, 0);
+                    return;
+                }
+                final Task task = gson.fromJson(json, Task.class);
+                final Integer id = task.getId();
+                if (id != null) {
+                    fbtm.updateTask(task);
+                    System.out.println("Обновили задачу id=" + id);
+                    httpExchange.sendResponseHeaders(200, 0);
+                } else {
+                    fbtm.addNewTask(task);
+                    System.out.println("Добавлена новая задача id=" + id );
+                    final String response = gson.toJson(task);
+                    sendText(httpExchange,response);
+                }
+            }
+
+        }
+    }
+
+    private void sendText(HttpExchange httpExchange, String response) throws IOException {
+        OutputStream os = httpExchange.getResponseBody();
+        httpExchange.getResponseHeaders().add("Content-Type","application/json;charset=utf-8");
+        httpExchange.sendResponseHeaders(200, 0);
+        os.write(response.getBytes(DEFAULT_CHARSET));
+        os.close();
+    }
+
+    private String readText(HttpExchange httpExchange) throws IOException {
+        InputStream is = httpExchange.getRequestBody();
+        httpExchange.sendResponseHeaders(201, 0);
+        return new String(is.readAllBytes(), DEFAULT_CHARSET);
+    }
+
+
+   /* static class TaskHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             OutputStream os = exchange.getResponseBody();
@@ -171,6 +285,6 @@ public class HttpTaskServer {
 
         }
 
-    }
+    }*/
 }
 
